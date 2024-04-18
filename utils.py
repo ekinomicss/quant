5f64fc0823453336ed62abf2448cdb1739b31708
ekinomicss <zorerekin@gmail.com>
2024-04-17 20:58:39 -0400

    add monthly stat computation and comments

--- utils.py
+++ utils.py
@@ -17,10 +17,10 @@ def plot_strategy_returns(cumulative_strategy_returns, title):
 
 
 def plot_skew(returns):
+    """ A histogram of returns, skewness in the title. """
     num_columns = 5
     fig, axes = plt.subplots(nrows=1, ncols=num_columns, figsize=(15, 5))
 
-    # Plot histogram for each column
     for i, column_name in enumerate(returns.columns):
         skewness= skew(returns[column_name])
         axes[i].hist(returns[column_name], bins=30, density=True, alpha=0.6)
@@ -28,20 +28,21 @@ def plot_skew(returns):
         axes[i].set_xlabel('Returns')
         axes[i].set_ylabel('Frequency')
 
-    # Adjust layout
     plt.tight_layout()
     plt.show()
     plt.show()
 
 
 def calc_summary_stats(returns, freq='daily'):
-    """Calculate summary statistics for returns."""
+    """ Calculate summary statistics for returns. """
     if freq == 'daily':
-        ann_factor = 252  # Annualization factor for daily returns
+        ann_factor = 252
     elif freq == 'weekly':
-        ann_factor = 52  # Annualization factor for weekly returns
+        ann_factor = 52
+    elif freq == 'monthly':
+        ann_factor = 12
     else:
-        raise ValueError("Invalid frequency. Supported frequencies are 'daily' and 'weekly'.")
+        raise ValueError("Invalid frequency. Supported frequencies are 'daily', 'weekly' and 'monthly'")
 
     rfr = 0.0462  # Risk-free rate as of 04.15.2024
 
@@ -49,8 +50,7 @@ def calc_summary_stats(returns, freq='daily'):
     mu = returns.mean()
     ann_vol = std * np.sqrt(ann_factor)
     cum_return = ((1+returns).cumprod().iloc[-1]/ (1+returns).cumprod().iloc[0])-1
-    num_trading_days = len(returns)
-    ann_return = returns.add(1).prod() ** (12 / num_trading_days) - 1
+    ann_return = returns.add(1).prod() ** (ann_factor) - 1
 
     excess_return = ann_return - rfr
     sharpe = excess_return / ann_vol
